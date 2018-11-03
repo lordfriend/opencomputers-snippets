1de8ef7716ac15212226c101c2d9ee0bf73247b5
lordfriend <lordfriend@outlook.com>
2018-11-03 13:37:06 +0800

    update tender resupply

--- tender_coal_resupply.lua
+++ tender_coal_resupply.lua
@@ -1,6 +1,7 @@
 local component = require("component");
 local sides = require("sides");
-local os = require("os");
+local keyboard = require("keyboard");
+local event = require("event");
 
 local ir_detector = component.ir_augment_detector;
 local rs = component.redstone;
@@ -15,16 +16,24 @@ local function detect()
         print("Overhead is not a tender");
         return false;
     end
-    if cargo_percent > 90 then
+    if cargo_percent > 80 then
         print("Overhead is nearly full");
         return false;
     end
 end
+
+function handleEvent(name, address, char, code, playerName)
+    if code == keyboard.keys.q then
+        isRunning = false;
+    end
+end
+
 if detect()
     rs.setOutput(sides.west, 1);
     print("Start resupply coals to ", ir_detector.info().name);
     while isRunning do
         isRunning = detect();
+        handleEvent(event.pull(10, "key_up"));
     end
     rs.setOutput(sides.west, 0);
     print("Resupply finished, please wait until the conveyor is empty");
