2a699f51838103c609e117127d3459da24a32bc6
lordfriend <lordfriend@outlook.com>
2018-11-05 00:51:43 +0800

    fix error

--- item_unloader_ctrl.lua
+++ item_unloader_ctrl.lua
@@ -2,14 +2,13 @@ local component = require("component");
 local event = require("event");
 local keyboard = require("keyboard");
 local sides = require("sides");
-local proxy = require("proxy");
 -- 0d411ba is redstone_card
 local redstone_io_addr = {};
 redstone_io_addr["1"] = component.get("63b63a"); 
 redstone_io_addr["2"] = component.get("fe5685");
 
 local function toggleActive(number, signal)
-    proxy = component.proxy(redstone_io_addr);
+    proxy = component.proxy(redstone_io_addr[number]);
     redstone_io_1.setOutput(sides.west, signal);
     redstone_io_1.setOutput(sides.east, signal);
     redstone_io_1.setOutput(sides.top, signal);
